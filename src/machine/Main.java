0c193d17942eaf3565881d7f88c2e3b18f8efe14
AmeenCodeBox <ameenmahagna365@gmail.com>
2025-06-20 22:44:00 +0200

    Add files via upload

---
+++ src/machine/Main.java
@@ -0,0 +1,10 @@
+package machine;
+
+import machine.controller.MainController;
+
+public class Main {
+    public static void main(String[] args) {
+        MainController controller = new MainController();
+        controller.start();
+    }
+}
